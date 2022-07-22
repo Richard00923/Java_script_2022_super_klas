0e4ffd3fe905d1690b6737f9d8d372fd6a334809
Richard00923 <vasyakov6@gmail.com>
2022-07-22 15:36:22 +0300

    ф

---
+++ lesson_1/main.js
@@ -0,0 +1,41 @@
+let hello = "hello";
+let owu = "owu";
+let com = "com";
+let ua = "ua";
+let number_1 = 1;
+let number_10 = 10;
+let number_999 = 999;
+let number_123 = 123;
+let number_P = 3.14;
+let number_fractional_1 = 2.7;
+let number_16 = 16;
+let boolean_1 = true;
+let boolean_2 = false;
+document.writeln(hello)
+document.writeln(owu)
+document.writeln(com)
+document.writeln(ua)
+console.log(number_1);
+console.log(number_fractional_1);
+console.log(number_16);
+console.log(boolean_1);
+console.log(boolean_2);
+alert(number_10);
+alert(number_999);
+alert(number_123);
+alert(number_P);
+let firstName = "Vasyl";
+let middleName = "Peter";
+let lastName = "Kovalchuk";
+let person = firstName + " " + middleName + " " + lastName;
+console.log(person);
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+let name = prompt("Please enter a name");
+let surname = prompt("Please enter your middle name");
+let age = prompt("Please enter age");
+console.log("my name "+name+" thanks to my father "+surname+" and my age "+age);
