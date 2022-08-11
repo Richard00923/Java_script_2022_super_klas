7cbde4caa643a95a56ad8bb7b406e91fbfaf42c6
Richard00923 <vasyakov6@gmail.com>
2022-08-11 14:29:50 +0300

    було непросто5

--- Lesson_6/master.js
+++ Lesson_6/master.js
@@ -42,26 +42,60 @@ let arr =str2.split(' ')
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
 let arr2=[10,8,-7,55,987,-1011,0,1050,0];
  let arr3 =arr2.map((value,)=>String(value))
-console.log(arr3);
+// console.log(arr3);
 
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-//     let nums = [11,21,3];
+   let nums = [11,21,3];
+
 // sortNums(nums,'ascending') // [3,11,21]
+nums.sort(function(a, b) {
+    return a - b;
+});
+// console.log(nums);
+
 // sortNums(nums,'descending') // [21,11,3]
-//
-//
+nums.sort(function(a, b) {
+    return b - a;
+});
+// console.log(nums);
+
 // - є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+
 // -- відсортувати його за спаданням за monthDuration
+// console.log(coursesAndDurationArray);
+let month= coursesAndDurationArray.sort(function(a, b) {
+    if (a.monthDuration > b.monthDuration) {
+        return 1;
+    }
+    if (a.monthDuration < b.monthDuration) {
+        return -1;
+    }
+
+    return 0;
+})
+console.log(month);
+let month1= coursesAndDurationArray.sort(function(a, b) {
+    if (b.monthDuration > a.monthDuration) {
+        return 1;
+    }
+    if (b.monthDuration < a.monthDuration) {
+        return -1;
+    }
+
+    return 0;
+})
+console.log(month1);
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-//
+
 // описати колоду карт
 // - знайти піковий туз
 // - всі шістки
@@ -81,7 +115,7 @@ console.log(arr3);
 // Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
 // {
 //     spades:[],
-//         diamonds:[],
+//     diamonds:[],
 //     hearts:[],
 //     clubs:[]
 // }
